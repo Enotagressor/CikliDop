0461e9b7622862a91c3bc40f6c5e6873bcf733b7
Sergey <enotagressor@gmail.com>
2022-04-29 18:38:04 +0700

    dop dz cikli

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dz!");
+        task2();
+    }
+    public static void task1(){
+        int i = 1;
+        int end = 30;
+        int n = 3;
+        int m = 5;
+        for (; i <= end; i++) {
+            System.out.print(i + ": ");
+            if (i % n == 0 && i % m == 0) {
+                System.out.print( "ping pong");
+            } else if (i % n == 0) {
+                System.out.print( "ping");
+            } else if (i % m == 0) {
+                System.out.print( "pong");
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void task2() {
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " " + b + " ");
+        for (int i = 3; i <= 10; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
     }
 }
